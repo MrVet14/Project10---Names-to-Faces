fa310cbc9fa3b38e708471b41810283428dc2c4e
Vitali <vitali.vyucheiski@gmail.com>
2022-06-13 12:11:13 +0300

    Changed Save & Load Methods
    Changed Save & Load Methods

--- Project10 - Names to Faces/ViewController.swift
+++ Project10 - Names to Faces/ViewController.swift
@@ -16,12 +16,15 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
         
         let defaults = UserDefaults.standard
-        
-        if let savePeople = defaults.object(forKey: "people") as? Data {
-            if let decodedPeople = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(savePeople) as? [Person] {
-                people = decodedPeople
+
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            let jsonDecoder = JSONDecoder()
+
+            do {
+                people = try jsonDecoder.decode([Person].self, from: savedPeople)
+            } catch {
+                print("Failed to load people")
             }
-                
         }
     }
 
@@ -68,8 +71,9 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         let person = Person(name: "Unknown", image: imageName)
         people.append(person)
-        collectionView.reloadData()
         save()
+        collectionView.reloadData()
+        
         dismiss(animated: true)
     }
     
@@ -97,9 +101,13 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     }
     
     func save() {
-        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: people, requiringSecureCoding: false) {
+        let jsonEncoder = JSONEncoder()
+        
+        if let savedData = try? jsonEncoder.encode(people) {
             let defaults = UserDefaults.standard
             defaults.set(savedData, forKey: "people")
+        } else {
+            print("Failed to save People")
         }
     }
 
